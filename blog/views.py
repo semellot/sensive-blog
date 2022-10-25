287e9e703083de40ff01a880a1a007027b40bdc0
Olga <o.panova27@gmail.com>
2022-10-25 23:45:23 +0300

    Show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from blog.models import Comment, Post, Tag
 def get_related_posts_count(tag):
     return tag.posts.count()
 
+def get_likes_count(post):
+    return post.likes.count()
 
 def serialize_post(post):
     return {
@@ -28,8 +30,10 @@ def serialize_tag(tag):
 
 
 def index(request):
-
-    most_popular_posts = []  # TODO. Как это посчитать?
+    
+    posts = Post.objects.all()
+    popular_posts = sorted(posts, key=get_likes_count, reverse=True)
+    most_popular_posts = popular_posts[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
